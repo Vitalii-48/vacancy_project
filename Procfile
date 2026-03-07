bca88a0d0d8eddcab00a837aa9fb79b6afe1db8a
Vitaliy Ivanov <ivanov.devwork@gmail.com>
2026-03-07 19:19:42 +0200

    Підготовлений для деплою на Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vacancy_project.wsgi
--- vacancy_project/settings.py
+++ vacancy_project/settings.py
@@ -1,4 +1,5 @@
 # vacancy_project\settings.pyfrom pathlib import Path
+
 from decouple import config
 from pathlib import Path
 
@@ -9,8 +10,7 @@ SECRET_KEY = config("V_SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 JOOBLE_API_KEY = config("JOOBLE_API_KEY")
 
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -19,12 +19,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "jobs",          # твій app для вакансій
+    "jobs",          # app для вакансій
     "telegram_bot",  # інтеграція з Telegram
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -53,13 +54,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "vacancy_project.wsgi.application"
 
-# 🔹 База SQLite
+# 🔹 База PostgreSQL
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "vacancy_sqlite3.db",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
@@ -68,11 +74,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = "uk-ua"
+LANGUAGE_CODE = "uk"
 TIME_ZONE = "Europe/Kiev"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
