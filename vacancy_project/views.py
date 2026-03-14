8d1d6a313ef6c080267ab159e87b5f08a0faacf7
Vitaliy Ivanov <ivanov.devwork@gmail.com>
2026-03-14 19:49:29 +0200

    Підготовлений для деплою на Render

--- vacancy_project/views.py
+++ vacancy_project/views.py
@@ -1,9 +1,12 @@
 # vacancy_project\views.py
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
+from decouple import config
 
+username = config('username_guest')
+password = config('password_guest')
 def guest_login(request):
-    user = authenticate(username='guest', password='guestpassword123')
+    user = authenticate(username=username, password=password)
     if user is not None:
         login(request, user)
     return redirect('/admin/jobs/vacancy/')
