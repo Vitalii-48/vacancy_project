2c86fcf61cc9931faee8db3673b26f5c179eb337
Vitaliy Ivanov <ivanov.devwork@gmail.com>
2026-04-09 16:27:19 +0300

    Add /ping/ endpoint for health check

---
+++ vacancy_project/admin.py
@@ -0,0 +1,8 @@
+# vacancy_project\admin.py
+
+from django.contrib import admin
+
+# Налаштування заголовків адмінки
+admin.site.site_header = "Адміністрування Vacancy Project"
+admin.site.site_title = "Vacancy"
+admin.site.index_title = "Django Project"
--- vacancy_project/urls.py
+++ vacancy_project/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from django.urls import path
 from django.shortcuts import redirect
 from .views import guest_login
+from django.http import HttpResponse
+import vacancy_project.admin
 
 urlpatterns = [
     path('', lambda request: redirect('/admin/')),
     path('admin/', admin.site.urls),
-    path('demo/', guest_login, name='guest_login')
+    path('demo/', guest_login, name='guest_login'),
+    path('ping/', lambda request: HttpResponse('ok'))
 ]
